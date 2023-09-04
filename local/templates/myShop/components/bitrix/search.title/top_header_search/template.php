64a8b5f683f07be508a9002a15af803cd486031e
Igor <qwertyhock@gmail.com>
2023-09-04 17:40:56 +0300

    04.09.23

--- local/templates/myShop/components/bitrix/search.title/top_header_search/template.php
+++ local/templates/myShop/components/bitrix/search.title/top_header_search/template.php
@@ -1,4 +1,4 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
+<? if (!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
 /** @var array $arParams */
 /** @var array $arResult */
 /** @global CMain $APPLICATION */
@@ -10,32 +10,50 @@
 /** @var string $templateFolder */
 /** @var string $componentPath */
 /** @var CBitrixComponent $component */
-$this->setFrameMode(true);?>
+$this->setFrameMode(true); ?>
 <?
 $INPUT_ID = trim($arParams["~INPUT_ID"]);
-if($INPUT_ID == '')
-	$INPUT_ID = "title-search-input";
+if ($INPUT_ID == '')
+    $INPUT_ID = "title-search-input";
 $INPUT_ID = CUtil::JSEscape($INPUT_ID);
 
 $CONTAINER_ID = trim($arParams["~CONTAINER_ID"]);
-if($CONTAINER_ID == '')
-	$CONTAINER_ID = "title-search";
+if ($CONTAINER_ID == '')
+    $CONTAINER_ID = "title-search";
 $CONTAINER_ID = CUtil::JSEscape($CONTAINER_ID);
 
-if($arParams["SHOW_INPUT"] !== "N"):?>
-	<div id="<?echo $CONTAINER_ID?>">
-	<form action="<?echo $arResult["FORM_ACTION"]?>">
-		<input id="<?echo $INPUT_ID?>" type="text" name="q" value="" size="40" maxlength="50" autocomplete="off" />&nbsp;<input name="s" type="submit" value="<?=GetMessage("CT_BST_SEARCH_BUTTON");?>" />
-	</form>
-	</div>
-<?endif?>
+if ($arParams["SHOW_INPUT"] !== "N"):?>
+    <div id="<? echo $CONTAINER_ID ?>" class="top_header_search">
+        <form action="<? echo $arResult["FORM_ACTION"] ?>">
+            <div class="search-container">
+                <input id="<? echo $INPUT_ID ?>" type="text" name="q" value="" size="40" placeholder="Поиск по каталогу"
+                       maxlength="50" autocomplete="off">
+                <button name="s" type="submit" class="search-button">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" fill="none">
+                        <g clip-path="url(#clip0_215_925)">
+                            <path d="M14 11L19 16L14 21" stroke="#A65CF0" stroke-width="2" stroke-linecap="round"
+                                  stroke-linejoin="round"/>
+                            <circle cx="16" cy="16" r="15" stroke="#A65CF0" stroke-width="2"/>
+                        </g>
+                        <defs>
+                            <clipPath id="clip0_215_925">
+                                <rect width="32" height="32" fill="white"/>
+                            </clipPath>
+                        </defs>
+                    </svg>
+                </button>
+            </div>
+        </form>
+    </div>
+<? endif ?>
 <script>
-	BX.ready(function(){
-		new JCTitleSearch({
-			'AJAX_PAGE' : '<?echo CUtil::JSEscape(POST_FORM_ACTION_URI)?>',
-			'CONTAINER_ID': '<?echo $CONTAINER_ID?>',
-			'INPUT_ID': '<?echo $INPUT_ID?>',
-			'MIN_QUERY_LEN': 2
-		});
-	});
+    BX.ready(function () {
+        new JCTitleSearch({
+            'AJAX_PAGE': '<?echo CUtil::JSEscape(POST_FORM_ACTION_URI)?>',
+            'CONTAINER_ID': '<?echo $CONTAINER_ID?>',
+            'INPUT_ID': '<?echo $INPUT_ID?>',
+            'MIN_QUERY_LEN': 2
+        });
+    });
 </script>
+
